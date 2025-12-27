63be0bfbe6e03d3b103e48da76d19bfcfb13c408
chandgadhiya rakshitkumar kishorbhai <rakshitchandgadhiya99@gmail.com>
2025-12-27 05:51:58 +0530

    factory design pattern

---
+++ creational/FactoryDesignPattern/FactoryDesignPattern.java
@@ -0,0 +1,61 @@
+package creational.FactoryDesignPattern;
+
+public class FactoryDesignPattern {
+    public static void main(String[] args) {
+        Creator creatorOfA = new concreteCreatorA();
+        Product ACreated = creatorOfA.createProduct();
+        ACreated.doStuff();
+    }
+
+}
+
+interface Product {
+    void doStuff();
+}
+
+class ConcreteProductA implements Product {
+
+    @Override
+    public void doStuff() {
+        System.out.println("doing some stuff through class concrete product A");
+    }
+
+}
+
+class ConcreteProductB implements Product {
+
+    @Override
+    public void doStuff() {
+        System.out.println("doing some stuff through class concrete product B");
+    }
+
+}
+
+abstract class Creator {
+    Product someOperation() {
+        System.out.println("doing some operation and returning Product object");
+        Product p = createProduct();
+        p.doStuff();
+        return p;
+    }
+
+    abstract Product createProduct();
+}
+
+class concreteCreatorA extends Creator {
+
+    @Override
+    Product createProduct() {
+        return new ConcreteProductA();
+    }
+
+}
+
+class concreteCreatorB extends Creator {
+
+    @Override
+    Product createProduct() {
+        return new ConcreteProductB();
+    }
+
+}
